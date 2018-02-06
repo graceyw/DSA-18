0396220c0668057b90e9eab54d24fdfe3190c380
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-02-05 23:48:15 -0500

    day03

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -13,16 +13,62 @@ public class Problems {
     }
 
     public static List<Integer> removeKDigits(int[] A, int k) {
-        // TODO: your code here
-        // For now, return a List that's correct size, but contains only 0s
-        List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
-        return l;
+        int size = A.length;   //this assumes that size == length ie list is full
+        if (size == 0){
+            throw new IndexOutOfBoundsException();
+        }
+
+        Stack<Integer> s = new Stack();
+        for (int i: A) {                //for each thing in A
+            while (s.size() != 0 && i < s.peek() && k > 0){
+                s.pop();
+                k--;
+            }
+            if (s.size() < A.length-k){
+                s.push(i);
+            }
+        }
+        LinkedList ll = new LinkedList<Integer>();
+        while (s.size() != 0){
+            ll.addFirst(s.pop());
+        }
+        return ll;
     }
 
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+        if (n == null){
+            return true;
+        }
+        Node x = n;
+        int size = 0;
+        while (x.next != null){
+            x = x.next;
+            size++;
+        }
+        Node tail = x;
+
+        int middleIndex = size/2;
+
+        Node currNode = n;
+        for (int i=0; i < middleIndex; i++) {    //find index of middle node
+            currNode = currNode.next;
+        }
+        Node middleNode = currNode;
+        Node m2 = middleNode.next;
+        for (int i = middleIndex; i < size; i++) {
+            Node mTemp = m2.next;                  // new temp node after n2
+            m2.next = middleNode;                  // redirect m2 pointer to middle
+            middleNode = m2;                       // reassign middlenode to next node
+            m2 = mTemp;                            // reassign m2 to next node
+        }
+        for (int i=0; i < middleIndex-1; i++) {
+            if (n.val != tail.val){
+                return false;
+            }
+            n = n.next;
+            tail = tail.next;
+        }
+        return true;
     }
 
     public static String infixToPostfix(String s) {
