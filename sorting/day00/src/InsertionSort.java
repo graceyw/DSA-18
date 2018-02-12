2d9539a102b714bab9c76e773c95cdc9fd5a2a5c
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-02-12 00:27:32 -0500

    insertionSort

--- sorting/day00/src/InsertionSort.java
+++ sorting/day00/src/InsertionSort.java
@@ -3,16 +3,22 @@ public class InsertionSort extends SortAlgorithm {
     /**
      * Use the insertion sort algorithm to sort the array
      *
-     * TODO
-     * Best-case runtime:
-     * Worst-case runtime:
-     * Average-case runtime:
+     * Best-case runtime: O(N)?
+     * Worst-case runtime: O(N^2)
+     * Average-case runtime: O(N)?
      *
-     * Space-complexity:
+     * Space-complexity: O(1) because no copies of the array are made
      */
     @Override
     public int[] sort(int[] array) {
-        // TODO
-        return new int[0];
+        for (int i=1; i < array.length; i++){
+            int elem = i;
+            while (array[elem] < array[elem-1] && elem > 0){
+                array[elem] = array[elem-1];  //switch the 2 elems
+                array[elem-1] = array[elem];  //switch the 2 elems
+                elem--;
+            }
+        }
+        return array;
     }
 }
