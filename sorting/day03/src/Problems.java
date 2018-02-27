3e7d0aa64ccaf25d53094f5602bd0d0048491cd7
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-02-26 21:36:30 -0500

    finish sorting03

--- sorting/day03/src/Problems.java
+++ sorting/day03/src/Problems.java
@@ -1,7 +1,15 @@
+import java.util.LinkedList;
+
 public class Problems {
 
     static void sortNumsBetween100s(int[] A) {
-        // TODO
+        for (int i=0; i<A.length; i++) {
+            A[i] = A[i] + 100;           //make range between 0 -> 200 instead of -100 -> 100
+        }
+        CountingSort.countingSort(A);
+        for (int i=0; i<A.length; i++) {
+            A[i] = A[i] - 100;           //bring it back to original range
+        }
     }
 
     /**
@@ -12,30 +20,42 @@ public class Problems {
         return s.charAt(s.length() - 1 - n) - 'a';
     }
 
-
     /**
      * Use counting sort to sort the String array according to a character
      *
      * @param n The digit number (where 0 is the least significant digit)
      */
     static void countingSortByCharacter(String[] A, int n) {
-        // TODO
+        LinkedList<String>[] L = new LinkedList[256];     //256 = total # of ASCII
+        for (int i = 0; i < 256; i++)
+            L[i] = new LinkedList<>();
+        for (String s : A) {
+            L[getNthCharacter(s,n)].add(s);               //Extract relevant digit from i, add i to the corresponding Linked List
+        }
+        int j = 0; // index in A to place numbers
+        for (LinkedList<String> list : L) {
+            while (list.size() > 0) {                     //Put all numbers in the linked lists into A
+                A[j] = list.pop();
+                j++;
+            }
+        }
     }
 
     /**
      * @param stringLength The length of each of the strings in S
      */
     static void sortStrings(String[] S, int stringLength) {
-        // TODO
+        for (int i=0; i<stringLength; i++) {
+            countingSortByCharacter(S,i);
+        }
     }
 
     /**
      * @param A The array to count swaps in
      */
 
-    public static int countSwaps(int[] A) {
+    public static int countSwaps(int[] A) {          //optional
         // TODO
         return 0;
     }
-
 }
--- sorting/day03/src/RadixSort.java
+++ sorting/day03/src/RadixSort.java
@@ -22,11 +22,14 @@ public class RadixSort {
         for (int i = 0; i < b; i++)
             L[i] = new LinkedList<>();
         for (int i : A) {
-            // TODO: Extract the relevant digit from i, and add i to the corresponding Linked List.
+            L[getNthDigit(i,b,n)].add(i);         //Extract relevant digit from i, add i to the corresponding Linked List
         }
         int j = 0; // index in A to place numbers
         for (LinkedList<Integer> list : L) {
-            // TODO: Put all numbers in the linked lists into A
+            while (list.size() > 0) {     //Put all numbers in the linked lists into A
+                A[j] = list.pop();
+                j++;
+            }
         }
     }
 
@@ -43,8 +46,10 @@ public class RadixSort {
         int k = A[0] + 1;
         for (int i = 1; i < A.length; i++)
             k = (A[i] + 1 > k) ? A[i] + 1 : k;
-        int w = (int) Math.ceil(Math.log(k) / Math.log(b)); // w = log base b of k, word length of numbers
-        // TODO: Perform radix sort
+        int w = (int) Math.ceil(Math.log(k) / Math.log(b));   // w = log base b of k, word length of numbers
+        for (int i=0; i<w; i++) {                             //Perform radix sort
+            countingSortByDigit(A,b,i);
+        }
     }
 
 }
