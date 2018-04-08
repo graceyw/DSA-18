e92d9378f99b83268f3e2c420d01db85ce3a25ca
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-04-07 20:07:25 -0400

    finally done

--- trees/day01/src/Problems.java
+++ trees/day01/src/Problems.java
@@ -1,16 +1,39 @@
 public class Problems {
 
+    //Runtime: O(N)
     public static int leastSum(int[] A) {
-        int[] countSort = new int[10];
-        int sum1 = 0;
-        int sum2 = 0;
-        for (int i=0; i<A.length; i++) {
-            countSort[A[i]]++;
+        if (A.length == 0) return 0;
+
+        int[] digitToCount = new int[10];  //because options are 0 -> 9
+        for (int i = 0; i < A.length; i++) {
+            digitToCount[A[i]]++;
         }
 
-        int
+        int[] sorted = new int[A.length];
 
+        int digit = 0;
+        int indexOfSorted = 0;
+        for (int count : digitToCount) {
+            while (count != 0) {
+                sorted[indexOfSorted] = digit;
+                indexOfSorted++;
+                count--;
+            }
+            digit++;
+        }
 
-        return sum1 +sum2;
+        String sum1str = "";
+        String sum2str = "";
+        for (int i = 0; i < sorted.length; i++) {
+            if (i % 2 == 0) {                //if i is even, so starting with index 0
+                sum1str += sorted[i];
+            } else {
+                sum2str += sorted[i];
+            }
+        }
+        if (sum2str.isEmpty()) {              //if sum2str is empty
+            return Integer.parseInt(sum1str); //just return sum1str
+        }
+        return Integer.parseInt(sum1str) + Integer.parseInt(sum2str);
     }
 }
