c96721e6e9dcd225aeee99827e853cc3f413791e
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-04-27 01:33:16 -0400

    done editDistance

--- DP/day01/src/EditDistance.java
+++ DP/day01/src/EditDistance.java
@@ -7,58 +7,38 @@
 public class EditDistance {
 
     public static int minEditDist(String a, String b) {
+        if (a.equals(b)) return 0;
+
         int[][] DP = new int[a.length()+1][b.length()+1];     //initialize memo
-        for (int i=0; i < DP.length-1; i++) {
-            for (int j = 0; j < DP.length-1; j++) {
-                DP[i][j] = -1;                             //set a special empty value
+        for (int i=0; i <= a.length(); i++) {
+            for (int j = 0; j <= b.length(); j++) {
+                DP[i][j] = -1;                                //set a special empty value
             }
         }
-        return recurse(a,b,DP);                 //call recursive function on problem to solve
+        return recurse(0,0,a,b,DP);                         //call recursive function on problem to solve
     }
 
 
     private static int recurse(int i, int j, String a, String b, int[][] DP) {
 
         // BASE CASES
-        if (i == a.length()) return j;                 //EditDistance =          because you must insert
-        if (j == b.length()) return i;                 //EditDistance =          because you must delete
+        if (i == a.length()) return b.length()-j;                 //EditDistance =          because you must insert
+        if (j == b.length()) return a.length()-i;                 //EditDistance =          because you must delete
 
         // CHECK MEMO
         if (DP[i][j] != -1) return DP[i][j];     // if already memoized, return that
 
+        //
+        if (a.charAt(i) == b.charAt(j)) {
+            int temp = recurse(i+1,j+1,a,b,DP);
+            DP[i][j] = temp;
+            return temp;
+        }
 
         //If a change must be made (insert, delete, or replace):
-        int tempMin = Math.min(DP[i+1][j],DP[i][j+1]);                // Necessary to do 2 bc Math.min only takes 2 inputs
-        DP[i][j] = 1 + Math.min(tempMin, DP[i+1][j+1]);    //Update Memo    //substring has exclusive ending index so check for off by 1 error
-        
+        int tempMin = Math.min(recurse(i+1,j,a,b,DP), recurse(i,j+1,a,b,DP));                // Necessary to do 2 bc Math.min only takes 2 inputs
+        DP[i][j] = 1 + Math.min(tempMin, recurse(i+1,j+1,a,b,DP));    //Update Memo    //substring has exclusive ending index so check for off by 1 error
 
-        //
-        if (a[a.length()].equals(b.(b.length()))) {       //check if A[0:i-1] == B[0:j] bc substring (inclusive,exclusive]
-            DP[a.length()][b.length()] = DP[a.length()][b.length()+1];
-        }
-        else if (a.substring(0,a.length()+1).equals(b.substring(0,b.length()))) {  //check if A[0:i] == B[0:j-1] bc substring (inclusive,exclusive]
-            DP[a.length()][b.length()] = DP[a.length()-1][b.length()];
-        }
-        else if (a.substring(0,a.length()).equals(b.substring(0,b.length()))) {    //check if A[0:i-1] == B[0:j-1] bc substring (inclusive,exclusive]
-            DP[a.length()][b.length()] = DP[a.length()-1][b.length()-1];
-        }
-
-//        for (int i=0; i<a.length(); i++) {          // Recurrence Relation  //check for off by 1 error, might be better to start at 0 and check that not 0
-//            for (int j=0; j<b.length(); j++) {
-//                if (a.substring(0,i).equals(b.substring(0,j+1))) {       //check if A[0:i-1] == B[0:j] bc substring (inclusive,exclusive]
-//                    DP[i][j] = DP[i][j-1];
-//                }
-//                else if (a.substring(0,i+1).equals(b.substring(0,j))) {  //check if A[0:i] == B[0:j-1] bc substring (inclusive,exclusive]
-//                    DP[i][j] = DP[i-1][j];
-//                }
-//                else if (a.substring(0,i).equals(b.substring(0,j))) {    //check if A[0:i-1] == B[0:j-1] bc substring (inclusive,exclusive]
-//                    DP[i][j] = DP[i-1][j-1];
-//                }
-//                //If a change must be made (insert, delete, or replace):
-//                int tempMin = Math.min(recurse(a.substring(0, i), b.substring(0, j - 1), DP), recurse(a.substring(0, i - 1), b.substring(0, j), DP));    //min(whole string, one less). Necessary to do 2 bc Math.min only takes 2 inputs
-//                DP[i][j] = 1 + Math.min(tempMin, recurse(a.substring(0, i), b.substring(0, j), DP));    //Update Memo    //substring has exclusive ending index so check for off by 1 error
-//            }
-//        }
-        return DP[a.length()][b.length()];
+        return DP[i][j];
     }
 }
