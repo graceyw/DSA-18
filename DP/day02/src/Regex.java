205579c9e23048382f1478e226b9ffd3ac8818a4
Gracey Wilson <gracey.wilson@students.olin.edu>
2018-05-07 01:49:12 -0400

    regex partial working - check for edge cases

--- DP/day02/src/Regex.java
+++ DP/day02/src/Regex.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Regex {
 
     /*
@@ -11,30 +13,32 @@ public class Regex {
         if (p == ".*") return true;
         if (s.length() == 0 || p.length() == 0) return (s.length() == p.length());   //if one is empty return false, but if both are empty return true
 
-        boolean[][] DP = new boolean[s.length()][p.length()];    //DP stores isMatch(s[:i],p[:j])
+        boolean[][] DP = new boolean[s.length()+1][p.length()+1];    //DP stores isMatch(s[:i],p[:j])
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < p.length(); j++) {
                 DP[i][j] = false;                                //just initializing them as all false
             }
         }
+        DP[0][0] = true;
 
         char[] string = s.toCharArray();
         char[] regex = p.toCharArray();
 
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < p.length(); j++) {
-
-                if (DP[i-1][j-1]) {
-                    if (regex[j] == '*') {
-                        DP[i][j] = DP[i-1][j-1];
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 1; j < p.length(); j++) {
+                if (regex[j-1] == '*' && (string[i-1]==regex[j-2] || regex[j-1] == '.')) {  //if the char in regex is "*" and (s[i]=p[j-1] or it COULD be (i.e. it's a ".")
+                    if (DP[i-1][j] || DP[i][j-1]) {
+                        DP[i][j] = true;
                     }
-                    if (regex[j] == '.' || string[i] == regex[j]) {
-                        DP[i][j] = DP[i-1][j-1];
+                }
+                if (DP[i-1][j-1]) {
+                    if (regex[j-1] == '.' || string[i-1] == regex[j-1]) {
+                        DP[i][j] = true;
                     }
                 }
             }
         }
-
-        return false;
+        System.out.println(Arrays.deepToString((DP)));
+        return DP[s.length()][p.length()];
     }
 }
